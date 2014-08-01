a807644c3f6aa1952f4ac74b948da3c307ec8349
Julien Coloos <julien.coloos@gmail.com>
2014-08-01 20:41:11 +0200

    Prepare 'Ask' window upon installation failure

---
+++ src/main/scala/usbinstall/InstallFailureController.scala
@@ -0,0 +1,39 @@
+package usbinstall
+
+import java.net.URL
+import java.util.ResourceBundle
+import javafx.event.ActionEvent
+import javafx.fxml.{FXML, Initializable}
+import javafx.scene.control.CheckBox
+import javafx.stage.Stage
+import usbinstall.settings.ErrorAction
+
+
+class InstallFailureController extends Initializable {
+
+  @FXML
+  protected var applyDefault: CheckBox = _
+
+  protected var action = ErrorAction.Ask
+
+  def getAction() = action
+
+  def getAsDefault() = applyDefault.isSelected()
+
+  override def initialize(fxmlFileLocation: URL, resources: ResourceBundle) {
+  }
+
+  def onStop(event: ActionEvent) {
+    action = ErrorAction.Stop
+    window.asInstanceOf[Stage].close()
+  }
+
+  def onContinue(event: ActionEvent) {
+    action = ErrorAction.Skip
+    window.asInstanceOf[Stage].close()
+  }
+
+  protected def window =
+    applyDefault.getScene().getWindow()
+
+}
