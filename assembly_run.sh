4374eb33ed0ff47c92e54db8bf89a891bb892c1a
Julien Coloos <julien.coloos@gmail.com>
2018-09-07 14:59:18 +0200

    Personal script to launch the application
    Generates assembly jar to be up-to-date.
    Runs as root (necessary for the commands that are to be used).
    Mounts the (personal) partition where ISOs are stored.
    Launches the application.

---
+++ assembly_run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -o nounset
+
+idGroup=
+idUser=
+
+# Restart program as root giving current user/group as parameter
+if [ $(id -u) -ne 0 ]
+then
+    sbt assembly
+    exec sudo $0 "##sudoed##" "$(id -un)" "$(id -gn)" "$@"
+fi
+
+if [ $# -gt 0 ] && [ "$1" = "##sudoed##" ]
+then
+    shift
+    idUser=$1
+    shift
+    idGroup=$1
+    shift
+fi
+
+
+# Under Arch, HOME is set according to running user. Which means it is /root
+# even upon sudo (unlike ubuntu which keeps the original user HOME).
+eval userHome=~${idUser:-root}
+export HOME=${userHome}
+
+# Make sure we have access to ISOs
+$(mount | grep -q win_d) || mount /mnt/win_d
+
+java -Dfile.encoding=UTF-8 -classpath src/main/resources:target/scala-2.12/usbinstall-assembly-0.0.3-SNAPSHOT.jar usbinstall.USBInstall
+
